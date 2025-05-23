2652640f2290478595525a3a82930eae0474fe06
Micheal Jabbour <135341866+MR-Michea1st@users.noreply.github.com>
2025-05-23 14:44:28 +0300

    Uploaded Files

---
+++ main.js
@@ -0,0 +1,33 @@
+let btn = document.querySelector('.add');
+let inp = document.querySelector('input');
+let f = 0;
+btn.addEventListener('click' , (ev) => {
+    let par = document.querySelector('.Todos');
+    if (inp.value.length > 0) {
+        let newDiv = document.createElement('div');
+        newDiv.textContent = inp.value;
+        newDiv.classList.add('card')
+        par.appendChild(newDiv)
+        let rem = document.createElement('img');
+        rem.src = '../Icons/rubbish-bin.png';
+        newDiv.appendChild(rem); 
+        rem.classList.add('Rubbish_icon');
+        let comp = document.createElement('img');
+        comp.src = '../Icons/blackTick.png';
+        newDiv.appendChild(comp);
+        comp.classList.add('tick_icon');
+        comp.onclick = (ev) => {
+            ev.target.parentNode.classList.toggle('checked');
+        }
+    
+        rem.onclick = (ev) => {
+            ev.target.parentNode.remove();
+        }
+    }
+    
+    inp.value = ''
+    
+})
+
+
+
